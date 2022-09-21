b0c9eb997e740a975d2dddd362107a0d926d8524
KLB0 <capalbo.lorenzo@tutanota.com>
2022-09-21 18:34:04 +0200

    Add responsive header

--- themes/portfolio/tailwind.config.js
+++ themes/portfolio/tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['content/**/*.md', 'layouts/**/*.html'],
   theme: {
-    extend: {},
+    colors: {
+      'primary': '#588157',
+      'secondary': '#3A5A40',
+      'text': '#344E41',
+      'container': '#A3B18A',
+      'background': '#DAD7CD'
+    },
+    extend: {
+      boxShadow: {
+        'menu': '0 -1px 20px rgba(0, 0, 0, 0.15)',
+      },
+      spacing: {
+        'menu': '-20rem',
+      }
+    },
   },
   plugins: [],
 }
