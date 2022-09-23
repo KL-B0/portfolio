82ca6ea62ad7387ddc057a283b4dc4159b1647da
KLB0 <capalbo.lorenzo@tutanota.com>
2022-09-23 20:18:25 +0200

    Refactor and polish skills section

--- themes/portfolio/assets/js/skills.js
+++ themes/portfolio/assets/js/skills.js
@@ -1,25 +1,11 @@
-const toggle1 = document.getElementById('skill-toggle1');
-const skill1 = document.getElementById('skill-card1');
-var open = false;
+const togglers = [];
+const cards = [];
+var states = [false, false, false, false]
 
-const togglers = [
-  document.getElementById('skill-toggle1'),
-  document.getElementById('skill-toggle2'),
-  document.getElementById('skill-toggle3'),
-  document.getElementById('skill-toggle4')
-];
-const cards = [
-  document.getElementById('skill-card1'),
-  document.getElementById('skill-card2'),
-  document.getElementById('skill-card3'),
-  document.getElementById('skill-card4'),
-]
-var states = [
-  false,
-  false,
-  false,
-  false
-]
+for (let i = 1; i <= 4; i++) {
+  togglers.push(document.getElementById('skill-toggle' + i));
+  cards.push(document.getElementById('skill-card' + i));
+}
 
 if (togglers && cards && states) {
   for (let i = 0; i < togglers.length; i++) {
@@ -28,18 +14,14 @@ if (togglers && cards && states) {
         states[i] = true;
 
         cards[i].classList.remove('hidden');
-        togglers[i].classList.remove('mb-4');
-        togglers[i].classList.remove('rounded-xl');
-        togglers[i].classList.add('rounded-t-xl');
+        //togglers[i].classList.remove('h-16');
         togglers[i].childNodes[5].classList.add('hidden');
         togglers[i].childNodes[7].classList.remove('hidden');
       } else {
         states[i] = false;
 
         cards[i].classList.add('hidden');
-        togglers[i].classList.add('mb-4');
-        togglers[i].classList.add('rounded-xl');
-        togglers[i].classList.remove('rounded-t-xl');
+        //togglers[i].classList.add('h-16');
         togglers[i].childNodes[5].classList.remove('hidden');
         togglers[i].childNodes[7].classList.add('hidden');
       }
