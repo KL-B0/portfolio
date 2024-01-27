8591a0e081c95b30ee02c7b3a3741c1546ed5349
KLB0 <capalbo.lorenzo@tutanota.com>
2024-01-27 19:19:56 +0100

    Add footer social links
    Add footer icons with social links, conforming with the website theme

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const TITLE = "Lorenzo Capalbo";
+export const DESCRIPTION = "I design and build software products";
+
+export const MAIL = "capalbo.lorenzo@tutanota.com"
+export const GITHUB_USER = "KL-B0"
+export const GITLAB_USER = "KL-B0"
+export const LINKEDIN_USER = 'lorenzo-capalbo-0424b3229'
