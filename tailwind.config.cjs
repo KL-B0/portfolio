507be4e7484622df4c7669107c80a04f94570684
Lorenzo Capalbo <capalbo.lorenzo@tutanota.com>
2024-02-24 19:21:36 +0100

    Update styling
    Update palette and change project styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,7 @@ export default {
         skin: {
           base: color("--color-text-base"),
           accent: color("--color-accent"),
-          inverted: color("--color-card"),
+          inverted: color("--color-fill"),
         },
       },
       backgroundColor: {
